e299ad5c7919b3e880006e71c0106d712c3f8349
clairewang0620-netizen <clairewang0620@gmail.com>
2025-12-14 13:20:07 +0800

    feat: Integrate MistakeBook and enhance quiz functionality
    This commit reintroduces and fully integrates the MistakeBook feature by enabling the `useMistakes` hook within the QuizDetail and MistakeBook components. Incorrect answers during quizzes are now automatically added to the MistakeBook.
    
    Additionally, this commit updates versioning information and cleans up the build process, reflecting a more stable release. The display of the version number has also been updated with a new color for better visibility.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@
 import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
-// import { MistakeProvider } from './contexts/MistakeContext';
-import { VocabularyProvider } from './contexts/VocabularyContext';
+import { MistakeProvider } from './contexts/MistakeContext';
 
 // Pages
-import HomePage from './pages/HomePage'; // Renamed to fix build casing issue
+import HomePage from './pages/HomePage'; // Using HomePage explicitly to fix build case-sensitivity
 import VocabularyList from './pages/VocabularyList';
 import Flashcard from './pages/Flashcard';
 import GrammarList from './pages/GrammarList';
@@ -19,40 +18,38 @@ import MistakeBook from './pages/MistakeBook';
 
 const App: React.FC = () => {
   return (
-    // <MistakeProvider>
-      <VocabularyProvider>
-        <HashRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              
-              {/* Vocabulary */}
-              <Route path="/vocabulary" element={<VocabularyList />} />
-              <Route path="/vocabulary/:level/:id" element={<Flashcard />} />
-              
-              {/* Grammar */}
-              <Route path="/grammar" element={<GrammarList />} />
-              
-              {/* Phrases */}
-              <Route path="/phrases" element={<PhrasesList />} />
-              
-              {/* Reading */}
-              <Route path="/reading" element={<ReadingList />} />
-              <Route path="/reading/:id" element={<ReadingDetail />} />
-              
-              {/* Quiz */}
-              <Route path="/quiz" element={<QuizList />} />
-              <Route path="/quiz/:level" element={<QuizDetail />} />
-              
-              {/* Mistakes */}
-              <Route path="/mistakes" element={<MistakeBook />} />
+    <MistakeProvider>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            
+            {/* Vocabulary */}
+            <Route path="/vocabulary" element={<VocabularyList />} />
+            <Route path="/vocabulary/:level/:id" element={<Flashcard />} />
+            
+            {/* Grammar */}
+            <Route path="/grammar" element={<GrammarList />} />
+            
+            {/* Phrases */}
+            <Route path="/phrases" element={<PhrasesList />} />
+            
+            {/* Reading */}
+            <Route path="/reading" element={<ReadingList />} />
+            <Route path="/reading/:id" element={<ReadingDetail />} />
+            
+            {/* Quiz */}
+            <Route path="/quiz" element={<QuizList />} />
+            <Route path="/quiz/:level" element={<QuizDetail />} />
+            
+            {/* Mistakes */}
+            <Route path="/mistakes" element={<MistakeBook />} />
 
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </Layout>
-        </HashRouter>
-      </VocabularyProvider>
-    // </MistakeProvider>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </MistakeProvider>
   );
 };
 
